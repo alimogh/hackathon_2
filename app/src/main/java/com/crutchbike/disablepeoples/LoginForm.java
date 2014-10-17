2d33db54a77da5c1b9d7b04e70fb1f0ee3c3b8a3
Андрей <jhonyxakep@qip.ru>
2014-10-18 01:31:31 +1100

    HTTP Client added

--- app/src/main/java/com/crutchbike/disablepeoples/LoginForm.java
+++ app/src/main/java/com/crutchbike/disablepeoples/LoginForm.java
@@ -6,9 +6,38 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.loopj.android.http.*;
+
+import org.apache.http.*;
 
 
 public class LoginForm extends Activity {
+
+    public class LoginHTTPClient {
+        private static final String BASE_URL = "http://private-anon-0bd7be832-hackathon4.apiary-mock.com/";
+
+        private AsyncHttpClient client = new AsyncHttpClient();
+
+        public void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+            client.get(getAbsoluteUrl(url), params, responseHandler);
+        }
+
+        public void get(String url, AsyncHttpResponseHandler responseHandler) {
+            client.get(getAbsoluteUrl(url), responseHandler);
+        }
+
+        public void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+            client.post(getAbsoluteUrl(url), params, responseHandler);
+        }
+
+        private String getAbsoluteUrl(String relativeUrl) {
+            return BASE_URL + relativeUrl;
+        }
+    }
+
+    LoginHTTPClient HTTPClient = new LoginHTTPClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +68,21 @@ public class LoginForm extends Activity {
 
     public void onLoginButton(View v)
     {
+        HTTPClient.get("/tasks", new TextHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseBody) {
+                Toast.makeText(getBaseContext(), responseBody,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseBody, Throwable e) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+            }
+
+        });
+
         Intent intent = new Intent(LoginForm.this, Maps.class);
         startActivity(intent);
     }
